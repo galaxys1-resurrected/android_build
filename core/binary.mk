20ef6f0b37cdd5cf0813f28920442fe69d25e02a
Cswl Coldwind <cswl1337@gmail.com>
2017-10-28 19:57:17 +0545

    allow building of targets without CLANG. needed for busybox
    Change-Id: I9448a6a90fedd9109c9b54949b36b15c061ead25

--- core/binary.mk
+++ core/binary.mk
@@ -376,11 +376,11 @@ ifneq ($(my_cc)$(my_cxx),)
 endif
 # Issue warning if LOCAL_CLANG* is set to false and the local makefile is not found
 # in the exception project list.
-ifeq ($(my_clang),false)
-    ifeq ($(call find_in_local_clang_exception_projects,$(LOCAL_MODULE_MAKEFILE))$(LOCAL_IS_AUX_MODULE),)
-        $(error $(LOCAL_MODULE_MAKEFILE): $(LOCAL_MODULE): LOCAL_CLANG is set to false)
-    endif
-endif
+# ifeq ($(my_clang),false)
+#     ifeq ($(call find_in_local_clang_exception_projects,$(LOCAL_MODULE_MAKEFILE))$(LOCAL_IS_AUX_MODULE),)
+#         $(error $(LOCAL_MODULE_MAKEFILE): $(LOCAL_MODULE): LOCAL_CLANG is set to false)
+#     endif
+# endif
 
 # clang is enabled by default for host builds
 # enable it unless we've specifically disabled clang above
